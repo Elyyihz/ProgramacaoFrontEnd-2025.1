e2183e8f244fbc6c42e311d95f943819de3fbf76
Elynne Silva de Lima <164261293+Elyyihz@users.noreply.github.com>
2025-02-14 00:38:20 +0000

    Texto01

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,9 +1,14 @@
+function Texto1(){
+return <p>Um outro paragrafo de texto!</p>  
+}
+
 export default function Home() {
   const aula = 2;
   return (
-    <>
+    <div>
     <h1> Oi Professor! </h1>
     <p> Essa é a nossa {aula}° aula de react!</p>
-    </>
+    <Texto1/>
+    </div>
   ) 
 }
